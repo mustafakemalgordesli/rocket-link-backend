1058d27a664102eefdbfd3b5b63a5495a805384d
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2022-10-14 00:47:03 +0300

    The path of the public file has been changed

--- src/scripts/utils/multer.js
+++ src/scripts/utils/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../../public/images"));
+    cb(null, path.join(__dirname, "../../../public/images"));
   },
   filename: function (req, file, cb) {
     cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
