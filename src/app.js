c6feef44a744bce268cdb529fb185e646af1723c
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2024-06-28 13:18:04 +0300

    Add cors policy

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+var cors = require('cors')
 const config = require("./config");
 const routes = require("./routes");
 const errorHandling = require("./middlewares/errorHandling");
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(cors())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
